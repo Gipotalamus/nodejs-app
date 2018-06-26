0bae15f26c4b19a467a24a2b9e03d09422a27808
gipotalamus <gipotalamus23@gmail.com>
2018-06-26 17:03:28 +0300

    test app first step

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let express = require('express');
+let app = express();
+let port = process.env.port || 8000;
+
+app.get('/', (req, res) => {
+	res.send({
+		name: 'John',
+		surname: 'Doe'
+	});
+})
+console.log(port);
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
