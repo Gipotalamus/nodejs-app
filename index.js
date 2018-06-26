c717fe5e8aedd1cf3213190b85262200b6f1a3d8
gipotalamus <gipotalamus23@gmail.com>
2018-06-26 17:38:12 +0300

    test app first step

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ let express = require('express');
 let app = express();
 let port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
+
+
+app.get('/json', (req, res) => {
 	res.send({
 		name: 'John',
 		surname: 'Doe'
 	});
 })
-console.log(port);
+
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/static/index.html');
+})
+
+
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
